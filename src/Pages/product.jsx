ee011192d2059a2a837c140fd8291e614ac0b59a
nuryudha <nur.yudha16@gmail.com>
2024-01-13 23:25:59 +0700

    VIP STUDIO CODE ( use Effect )
    undefined

--- src/Pages/product.jsx
+++ src/Pages/product.jsx
@@ -1,14 +1,15 @@
-import { Fragment } from "react";
-import CardProduct from "../components/Fragments/CardProduct";
-import Button from "../components/Elements/Button";
-import Counter from "../components/Fragments/Counter";
+import { Fragment, useEffect, useState } from 'react';
+
+import Button from '../components/Elements/Button';
+import CardProduct from '../components/Fragments/CardProduct';
+import Counter from '../components/Fragments/Counter';
 
 const products = [
   {
     id: 1,
-    name: "Sepatu Baru",
-    price: "Rp 1.000.000",
-    image: "/images/shoes-1.jpg",
+    name: 'Sepatu Baru',
+    price: 1000000,
+    image: '/images/shoes-1.jpg',
     description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt nam
     impedit labore magnam inventore veniam praesentium voluptatem facere
     cumque eveniet? Aspernatur eaque incidunt provident officia deleniti!
@@ -16,31 +17,55 @@ const products = [
   },
   {
     id: 2,
-    name: "Sepatu Lama",
-    price: "Rp 500.000",
-    image: "/images/shoes-1.jpg",
+    name: 'Sepatu Lama',
+    price: 500000,
+    image: '/images/shoes-1.jpg',
     description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit.`,
   },
   {
     id: 3,
-    name: "Sepatu an",
-    price: "Rp 200.000",
-    image: "/images/shoes-1.jpg",
+    name: 'Sepatu ',
+    price: 200000,
+    image: '/images/shoes-1.jpg',
     description: `Lorem ipsum dolor, sit amet adipisicing elit.`,
   },
 ];
 
-const email = localStorage.getItem("email");
+const email = localStorage.getItem('email');
 
 function ProductPage(props) {
+  const [cart, setCart] = useState([]); // disini parameter item
+  const [totalPrice, setTotalPrice] = useState(0);
 
-  const [cart, setCart]
+  useEffect(() => {
+    // didmount
+    setCart(JSON.parse(localStorage.getItem('cart')) || []);
+  }, []); // kalau kosong depedencynya jadi DIDMOUNT
 
+  useEffect(() => {
+    // didupdate
+    if (cart.length > 0) {
+      const sum = cart.reduce((acc, item) => {
+        const product = products.find((product) => product.id === item.id);
+        return acc + product.price * item.quantity;
+      }, 0);
+      setTotalPrice(sum);
+      localStorage.setItem('cart', JSON.stringify(cart));
+    }
+  }, [cart]); // perubahan apa yang akan kita pantau, jadi ketika cart berubah maka sya akan mengupdate total price
 
   function handleLogout() {
-    localStorage.removeItem("email");
-    localStorage.removeItem("password");
-    window.location.href = "/login";
+    localStorage.removeItem('email');
+    localStorage.removeItem('password');
+    window.location.href = '/login';
+  }
+
+  function handleAddToCart(id) {
+    if (cart.find((item) => item.id === id)) {
+      setCart(cart.map((item) => (item.id === id ? { ...item, quantity: item.quantity + 1 } : item)));
+    } else {
+      setCart([...cart, { id, quantity: 1 }]);
+    }
   }
 
   return (
@@ -52,21 +77,51 @@ function ProductPage(props) {
         </Button>
       </div>
       <div className="flex justify-center py-5">
-        <div className="w-3/4 flex flex-wrap">
+        <div className="w-4/6 flex flex-wrap">
           {products.map((product) => (
             <CardProduct key={product.id}>
               <CardProduct.Header image={product.image} />
-              <CardProduct.Body title={product.name}>
-                {product.description}
-              </CardProduct.Body>
-              <CardProduct.Footer price={product.price} />
+              <CardProduct.Body name={product.name}>{product.description}</CardProduct.Body>
+              <CardProduct.Footer price={product.price} id={product.id} handleAddToCart={handleAddToCart} />
             </CardProduct>
           ))}
         </div>
-        <div className="w-1/4">
-          <h1 className="text-3xl font-bold">Cart</h1>
+        <div className="w-2/6">
+          <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-2">Cart</h1>
+          <table className="text-left table-auto border-separate border-spacing-x-5">
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item) => {
+                const product = products.find((product) => product.id === item.id);
+                return (
+                  <tr key={item.id}>
+                    <td>{product.name}</td>
+                    <td>{product.price.toLocaleString('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</td>
+                    <td>{item.quantity}</td>
+                    <td>{(item.quantity * product.price).toLocaleString('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</td>
+                  </tr>
+                );
+              })}
+              <tr>
+                <td colSpan={3} className="font-bold">
+                  Total Price
+                </td>
+                <td className="font-bold"> {totalPrice.toLocaleString('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0, maximumFractionDigits: 0 })} </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
+      {/* <div className="mt-5 mb-5 flex justify-center">
+        <Counter></Counter>
+      </div> */}
     </Fragment>
   );
 }
--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -1,12 +1,8 @@
-import Button from "../Elements/Button";
+import Button from '../Elements/Button';
 
 function CardProduct(props) {
   const { children } = props;
-  return (
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 my-2 flex flex-col justify-between">
-      {children}
-    </div>
-  );
+  return <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-3 my-2 flex flex-col justify-between">{children}</div>;
 }
 
 function Body(props) {
@@ -14,9 +10,7 @@ function Body(props) {
   return (
     <div className="px-5 pb-5 h-full">
       <a href="">
-        <h5 className="text-xl font-semibold tracking-tight text-white">
-          {name}
-        </h5>
+        <h5 className="text-2xl font-semibold tracking-tight text-white mb-5">{name}</h5>
         <p className="text-m text-white">{children}</p>
       </a>
     </div>
@@ -33,11 +27,13 @@ function Header(props) {
 }
 
 function Footer(props) {
-  const { price } = props;
+  const { price, handleAddToCart, id } = props;
   return (
     <div className="flex item-center justify-between px-5 pb-5">
-      <span className="text-xl font-bold text-white">{price}</span>
-      <Button classname="bg-blue-600">Add To Chart</Button>
+      <span className="text-xl font-bold text-white">{price.toLocaleString('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</span>
+      <Button classname="bg-blue-600" onClick={() => handleAddToCart(id)}>
+        Add To Chart
+      </Button>
     </div>
   );
 }
